925b58a4c27148da0206bd2c0518e0368ee62f93
KIKonstantin <92094598+KIKonstantin@users.noreply.github.com>
2022-08-12 11:22:20 +0300

    Delete rounding.js

--- rounding.js
+++
@@ -1,8 +0,0 @@
-function rounding(num, presicion) {
-  if (presicion > 15) {
-    presicion = 15;
-  }
-  num = num.toFixed(presicion);
-  console.log(parseFloat(num));
-}
-rounding(3.1415926535897932384626433832795, 2);
