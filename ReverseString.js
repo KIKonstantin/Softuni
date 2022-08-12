03fa35d514fafe38fe5ee128d4d3ee709886ea57
KIKonstantin <92094598+KIKonstantin@users.noreply.github.com>
2022-08-12 11:00:26 +0300

    Delete ReverseString.js

--- ReverseString.js
+++
@@ -1,7 +0,0 @@
-function ReverseString(word){
-    let splitstring = word.split("");
-    let revstri = splitstring.reverse();
-    let joinArray = revstri.join("");
-    console.log(joinArray);
-}
-ReverseString("Split")
